62b22c71ec10c980bb95765b4d691dcd1bc79afe
Alewi Boluwatife <alewibolu05@gmail.com>
2023-11-22 16:03:41 +0100

    first commit for review_pogp

--- models/load_pearl_of_great_price.py
+++ models/load_pearl_of_great_price.py
@@ -7,6 +7,26 @@ from models.load_utils import clean
 from models.load_utils import to_markdown
 
 
+def remove_hrefs_and_strong_text(html_content):
+    """Remove hrefs in p tag."""
+    soup = BeautifulSoup(html_content, "html.parser")
+    # Find all <p> tags containing <a> tags with href attributes and <em> tags
+    for p_tag in soup.find_all("p"):
+        a_tags = p_tag.find_all("a", href=True)
+        for a_tag in a_tags:
+            # Find <strong> tags within <em> tags and remove them
+            em_tag = a_tag.find("em")
+            if em_tag:
+                strong_tag = em_tag.find("strong")
+                if strong_tag:
+                    strong_tag.extract()
+            # Remove the entire <a> tag
+            a_tag.extract()
+    # Get the cleaned HTML content
+    cleaned_content = str(soup)
+    return cleaned_content
+
+
 def load_pogp(url: str, html: str, bs_parser: str = "html.parser") -> Document:
     """Load Pearl of Great Price from a url and html."""
     soup = BeautifulSoup(html, bs_parser)
@@ -15,6 +35,7 @@ def load_pogp(url: str, html: str, bs_parser: str = "html.parser") -> Document:
 
     content = clean(to_markdown(str(body), base_url=url)) if body else ""
     content = content.split("\n\n#### Further Reading\n\n")[0]
+    content = remove_hrefs_and_strong_text(content)
     metadata = {
         "url": url,
         "title": clean(title) if title else "",
